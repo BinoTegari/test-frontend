1082b7704da08c014d4cd6e92abfd53a2de04380
Andrei <binofaata@gmail.com>
2020-08-11 19:39:00 +0400

    старое

--- src/app/order-form/order-form-routing.module.ts
+++ src/app/order-form/order-form-routing.module.ts
@@ -1,7 +1,7 @@
 import { OrderDetailComponent } from './components/order-detail/order-detail.component';
 import { OrderFormComponent } from './components/order-form.component';
 import { OrderFenceDetailComponent } from './components/order-fence-detail/order-fence-detail.component';
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { OrderCustomerDetailComponent } from './components/order-customer-detail/order-customer-detail.component';
 
